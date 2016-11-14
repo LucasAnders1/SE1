1664ebcc841252eb594bc61c611c7e456603334a
LyHa05 <lydia.pflug@web.de>
2016-11-14 09:43:30 +0100

    Repo Aufgabe und ToDoListe erweitert

--- src/main/java/se1app/applicationcore/repos/AufgabeRepo.java
+++ src/main/java/se1app/applicationcore/repos/AufgabeRepo.java
@@ -1,11 +1,14 @@
 package se1app.applicationcore.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import se1app.applicationcore.Aufgabe;
+import se1app.applicationcore.ToDoListe;
 
 public interface AufgabeRepo extends JpaRepository<Aufgabe, Integer>{
 	
-	
+	List<Aufgabe> findAufgabenByList(ToDoListe liste);
 
 }
--- src/main/java/se1app/applicationcore/repos/ToDoListeRepo.java
+++ src/main/java/se1app/applicationcore/repos/ToDoListeRepo.java
@@ -1,9 +1,13 @@
 package se1app.applicationcore.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import se1app.applicationcore.Benutzer;
 import se1app.applicationcore.ToDoListe;
 
 public interface ToDoListeRepo extends JpaRepository<ToDoListe, Integer> {
 
+	List<ToDoListe> findListsByBenutzer(Benutzer benutzerNr);
 }
