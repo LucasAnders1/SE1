33ad091ccfbc5e4dca2bd962090c9bc1b0fc738f
LyHa05 <lydia.pflug@web.de>
2016-11-14 11:01:06 +0100

    Datentyp um LocalDate erweitert

--- src/main/java/se1app/applicationcore/util/DatumTyp.java
+++ src/main/java/se1app/applicationcore/util/DatumTyp.java
@@ -1,12 +1,17 @@
 package se1app.applicationcore.util;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class DatumTyp implements Serializable {
 
-	public DatumTyp(DatumTyp termin) {
-		// TODO Copy-Constructor erstellen
+	public DatumTyp(DatumTyp datumTyp) {
+		this.datum = datumTyp.datum;
 	}
 
-	// TODO implementieren
+	public DatumTyp(LocalDateTime datum) {
+		this.datum = datum;
+	}
+	
+	private LocalDateTime datum;
 }
--- src/main/java/se1app/applicationcore/util/TimestampTyp.java
+++ src/main/java/se1app/applicationcore/util/TimestampTyp.java
@@ -1,11 +1,18 @@
 package se1app.applicationcore.util;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class TimestampTyp implements Serializable{
 
-	public TimestampTyp(TimestampTyp erstelldatum) {
-		// TODO Copy-Constructor erstellen
+	public TimestampTyp(TimestampTyp timestampTyp) {
+		this.zeitStempel = timestampTyp.zeitStempel;
 	}
+	
+	public TimestampTyp(LocalDateTime zeitStempel) {
+		this.zeitStempel = zeitStempel;
+	}
+	
+	private LocalDateTime zeitStempel;
 
 }
