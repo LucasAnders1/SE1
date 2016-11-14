1f477247c4210818a47ec7ab3c7284b301901edf
LyHa05 <lydia.pflug@web.de>
2016-11-14 09:42:25 +0100

    Datentypen Datum und Timestamp blanko erstellt

---
+++ src/main/java/se1app/applicationcore/util/DatumTyp.java
@@ -0,0 +1,12 @@
+package se1app.applicationcore.util;
+
+import java.io.Serializable;
+
+public class DatumTyp implements Serializable {
+
+	public DatumTyp(DatumTyp termin) {
+		// TODO Copy-Constructor erstellen
+	}
+
+	// TODO implementieren
+}
---
+++ src/main/java/se1app/applicationcore/util/TimestampTyp.java
@@ -0,0 +1,11 @@
+package se1app.applicationcore.util;
+
+import java.io.Serializable;
+
+public class TimestampTyp implements Serializable{
+
+	public TimestampTyp(TimestampTyp erstelldatum) {
+		// TODO Copy-Constructor erstellen
+	}
+
+}
