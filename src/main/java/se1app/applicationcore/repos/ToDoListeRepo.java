44bd969387caf3cfbeb981a8768594b872c39d26
Dennis <Dennis.Pietruck@haw-hamburg.de>
2016-11-14 13:23:17 +0100

    findListsByBenutzer implementiert

--- src/main/java/se1app/applicationcore/repos/ToDoListeRepo.java
+++ src/main/java/se1app/applicationcore/repos/ToDoListeRepo.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se1app.applicationcore.Benutzer;
 import se1app.applicationcore.ToDoListe;
 
 public interface ToDoListeRepo extends JpaRepository<ToDoListe, Integer> {
 
-	List<ToDoListe> findListsByBenutzer(Benutzer benutzerNr);
+	@Query(value = "SELECT * FROM TODOLISTE WHERE EXISTS(SELECT * FROM BENUTZER_HATLISTEN WHERE TODOLISTE.ID = BENUTZER_HATLISTEN.TODOLISTE_ID AND BENUTZER_HATLISTEN.BENUTZERID := benutzerNr)", nativeQuery = true)
+	List<ToDoListe> findListsByBenutzer(@Param("benutzerNr") Benutzer benutzerNr);
 }
