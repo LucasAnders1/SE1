5dc28ed82c82ecd7b74c9eb75b799b8847e9fac1
Lydia Pflug <Lydia Pflug>
2016-11-15 12:14:30 +0100

    Timestamp

--- src/main/java/se1app/applicationcore/util/TimestampTyp.java
+++ src/main/java/se1app/applicationcore/util/TimestampTyp.java
@@ -3,16 +3,16 @@ package se1app.applicationcore.util;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class TimestampTyp implements Serializable{
+public class TimestampTyp implements Serializable {
 
 	public TimestampTyp(TimestampTyp timestampTyp) {
 		this.zeitStempel = timestampTyp.zeitStempel;
 	}
-	
-	public TimestampTyp(LocalDateTime zeitStempel) {
-		this.zeitStempel = zeitStempel;
+
+	public TimestampTyp() {
+		this.zeitStempel = LocalDateTime.now();
 	}
-	
+
 	private LocalDateTime zeitStempel;
 
 }
